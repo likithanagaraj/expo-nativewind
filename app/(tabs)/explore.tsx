903bbdc6aaabc75c2ae651074ba708c51a88ea11
Likitha N <likithanagaraj2004@gmail.com>
2025-06-22 12:11:53 +0530

    Refactor ExploreScreen and HomeScreen components; replace ScrollView with SafeAreaView and improve layout structure

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,17 +1,18 @@
 // ExploreScreen.tsx
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ExploreScreen() {
   return (
-    <ScrollView className="flex-1 bg-white p-8 ">
-      <Text className="text-3xl font-bold text-gray-900 mb-4">
-         Explore 🔍
-      </Text>
-
-      <Text className="text-lg text-gray-700 mb-4">
-        Its Explore Screen! 
-      </Text>
-    </ScrollView>
+    <SafeAreaView className="flex-1 p-5">
+          <View className="  ">
+            <Text className="text-3xl font-bold text-gray-900 mb-4">
+              🔍 Explore
+            </Text>
+    
+            <Text className="text-lg text-gray-700 mb-4">Its Explore Screen!</Text>
+          </View>
+        </SafeAreaView>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,18 @@
 // HomeScreen.tsx
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <ScrollView className="flex-1 bg-white p-8 ">
-      <Text className="text-3xl font-bold text-gray-900 mb-4">
-        Welcome Back 👋
-      </Text>
+    <SafeAreaView className="flex-1 p-5">
+      <View className="  ">
+        <Text className="text-3xl font-bold text-gray-900 mb-4">
+          Welcome Back 👋
+        </Text>
 
-      <Text className="text-lg text-gray-700 mb-4">
-        Its Home Screen! 
-      </Text>
-    </ScrollView>
+        <Text className="text-lg text-gray-700 mb-4">Its Home Screen!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
