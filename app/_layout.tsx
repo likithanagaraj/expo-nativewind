cd0c6d5fbad129114b861757e3a4fefde8322ffa
Likitha N <likithanagaraj2004@gmail.com>
2025-06-27 22:38:15 +0530

    Added the react-native animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import "react-native-reanimated";
 import "../global.css";
-
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
@@ -15,7 +15,7 @@ export default function RootLayout() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView className="flex-1 ">
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="create-note/[id]" options={{ headerShown: false }} />
@@ -23,6 +23,6 @@ export default function RootLayout() {
       </Stack>
 
       <StatusBar style="light" />
-    </>
+    </GestureHandlerRootView>
   );
 }
